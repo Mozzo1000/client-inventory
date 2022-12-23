f5fef66b09010753e4ac768f5cad0fb26cb7d6ea
Andreas Backström <mozzo242@gmail.com>
2022-12-23 15:39:06 +0100

    Add material-ui-snackbar-provider

--- web/src/index.js
+++ web/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SnackbarProvider } from 'material-ui-snackbar-provider'
+import Alert from './components/Alerts/Alert';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider SnackbarComponent={Alert}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>
 );
