ba52eb343118a95847a55b4aff766738df9c2d57
Andreas Backström <mozzo242@gmail.com>
2023-01-01 22:31:19 +0100

    Migrate from CRA to vite

--- web/src/services/auth.service.js
+++ web/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_CLIENT_INVENTORY_API_URL;
+const BASE_URL = import.meta.env.VITE_CLIENT_INVENTORY_API_URL;
 const API_URL = BASE_URL + "/v1/auth/";
 
 const register = (email, name, password) => {
--- web/src/services/stats.service.js
+++ web/src/services/stats.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const BASE_URL = process.env.REACT_APP_CLIENT_INVENTORY_API_URL;
+const BASE_URL = import.meta.env.VITE_CLIENT_INVENTORY_API_URL;
 const API_URL = BASE_URL + "/v1/stats";
 
 const get = () => {
